0a439e44ee6297a6bef28945c67f3cdb8eab7318
Florian <mrvdpflorian@gmail.com>
2021-12-02 18:02:37 +0100

    tweeter

--- exos/4_async/index.js
+++ exos/4_async/index.js
@@ -7,19 +7,15 @@ console.log('--- Question 1 ---');
   Si ce nombre est pair, le résoudre, sinon le rejeter.
   Utiliser setTimeout().
 */
-const myPromise = new Promise(function (resolve, reject){
-    setTimeout(function (){
+const myPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         const myRandomNumber = Math.floor(Math.random() * 100);
-        if (myRandomNumber%2 === 0){
+        if (myRandomNumber % 2 === 0) {
             resolve(myRandomNumber + " est paire !");
-        }else
+        } else
             reject(myRandomNumber + " n'est pas paire !")
     }, 2000)
 })
-myPromise.then(function (myNumber){
-    console.log(myNumber)
-})
-
 console.log('--- Question 2 ---');
 /* Consommer la Promesse de 1) pour
   - afficher le nombre renvoyé quand elle résoud
@@ -27,17 +23,48 @@ console.log('--- Question 2 ---');
   - logguer 'Fin' quoi qu'il arrive
 */
 
+myPromise.then(function (myNumber) {
+    //console.log("Then: " + myNumber)
+}).catch(function (myNumber) {
+    //console.log("Catch: Pas paire !")
+}).finally(function (myNumber) {
+    //console.log("Finally: Oh Yeaaaah !")
+})
+
 console.log('--- Question 3 ---');
 /* Créer une promesse en utilisant fetch(), et la consommer pour afficher la donnée contenue dans l'url ci-dessous:
  */
 const url =
-  'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json';
+    'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json';
+fetch(url).then(
+    function (maReponse) {
+        //console.log(maReponse.json());
+    }
+)
+fetch(url).catch(
+    function (maDonnee) {
+        //console.log("maDonnee", maDonnee);
+    }
+)
 
 console.log('--- Question 4 ---');
 /* Écrire une fonction getJson() qui prend une URL en entrée,
   et renvoie la Promesse de récupérer la réponse HTTP parsée depuis du JSON
   Utiliser getJson pour refaire la question 3.
 */
+function getJson(url){
+    return fetch(url).then(
+        function (response) {
+            return response.json();
+        }
+
+    ).catch(
+        function (error) {
+            console.error(error)
+        }
+    )
+}
+console.log(getJson(url));
 
 console.log('--- BONUS ---');
 /* Créer une Promesse qui:
--- src/index.js
+++ src/index.js
@@ -1,57 +1,157 @@
 'use strict';
+function getJson(url) {
+    return fetch(url)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+}
 
 document.addEventListener(
-  'DOMContentLoaded',
-  async function () {
-    const resp = await fetch(
-      'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json',
-    );
-    const tweets = await resp.json();
-    console.log('Le tableau de tweet', tweets);
+    'DOMContentLoaded',
+    async function () {
+        const promise1 = getJson('https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets2.json')
+        const promise2 = getJson('https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json')
+        const tweets = (await Promise.all([promise1, promise2])).flat();
 
-    // ### Projet Touitter ###
+        console.log('Le tableau de tweet', tweets.flat());
 
-    // [0] observer la structure de donnée des tweets dans la console de votre navigateur
+        // ### Projet Touitter ###
 
-    // [1] créer et ajouter un <button> "bouton" qui quand on clique dessus affiche 'click' dans la console.
-    // Ne pas oublier de donner un textContent au bouton
+        // [0] observer la structure de donnée des tweets dans la console de votre navigateur
 
-    // [2] créer une fonction, qui pour un tweet en entrée, crée et retourne un <li> contenant le texte du tweet
+        // [1] créer et ajouter un <button> "bouton" qui quand on clique dessus affiche 'click' dans la console.
+        // Ne pas oublier de donner un textContent au bouton
+        function createButton() {
+            const buttonDisplayClick = document.createElement("button");
+            buttonDisplayClick.textContent = "Fr";
+            buttonDisplayClick.addEventListener(
+                "click",
+                () => {
+                    if (!idFr) {
+                        idFr = true;
+                        buttonDisplayClick.textContent = "Tous";
+                        const arrayFilter = tweets.filter(item => item.lang === "fr");
+                        const filteredOL = displayTweetList(arrayFilter)
+                        displayedOL.replaceWith(filteredOL);
+                        displayedOL = filteredOL;
 
-    // [3] créer et ajouter un <ol> à la page, puis y ajouter les <li> de tweets en utilisant [2]
+                    } else {
+                        idFr = false;
+                        buttonDisplayClick.textContent = "Fr";
+                        const allOl = displayTweetList(tweets);
+                        displayedOL.replaceWith(allOl);
+                        displayedOL = allOl;
 
-    // [4] créer une fonction checkFr qui pour un tweet en entrée renvoie vrai ou faux selon si la langue est 'fr'
+                    } //endElse
+                } // listener
+            ) // eventListener
 
-    // [5] modifier "bouton", et utiliser checkFr au clic du bouton pour afficher dans la console la liste des tweets français
+            return buttonDisplayClick;
+        }
 
-    // [6] modifier "bouton" pour que quand on clique dessus,
-    //  - crée un nouveau <ol> avec seulement les tweets français
-    //  - remplace le <ol> existant avec le nouvel <ol>, en utilisant .replaceWith()
+        document.body.append(createButton());
 
-    // [7] créer une variable booléenne isFr, et l'initialiser à false
+        // [2] créer une fonction, qui pour un tweet en entrée, crée et retourne un <li> contenant le texte du tweet
+        function createLi(tweet) {
+            const theLiTweet = document.createElement("li");
+            // Auteur
+            if (tweet.user.profile_image_url_https){
+                const image = document.createElement("img");
+                image.src = tweet.user.profile_image_url_https;
+                theLiTweet.append(image)
+            }
+            const autor = document.createElement("a");
+            autor.textContent = tweet.user.name;
+            theLiTweet.append(autor)
+            const autorReal = document.createElement("span");
+            autorReal.textContent = "@"+tweet.user.screen_name;
+            theLiTweet.append(autorReal)
 
-    // [8] inverser la valeur du booléen au clic sur "bouton"
+            // Content
+            const content = document.createElement("p");
+            content.textContent = tweet.full_text;
+            theLiTweet.append(content)
 
-    // [9] modifier "bouton" pour pouvoir réafficher tous les tweets quand on reclique dessus
-    //  Il faut utiliser isFr.
+            // Favoris
+            theLiTweet.append(buttonFav())
 
-    // [10] changer l'intitulé de "bouton" de filtre en fonction de isFr
+            return theLiTweet;
+        }
 
-    /* [11] créer une fonction, qui pour un tableau tweets en entrée, crée et retourne un <ol> rempli de <li>
-    et l'utiliser à [3], [6], [9] */
+        // [3] créer et ajouter un <ol> à la page, puis y ajouter les <li> de tweets en utilisant [2]
+        function createOlList() {
+            const olList = document.createElement("ol");
+            document.body.append(olList)
+            return olList;
+        }
+/*
+        const displayedOL = createOlList();
+        tweets.forEach((tweet) => {
+            displayedOL.append(createLi(tweet))
+        })
+*/
+        // [4] créer une fonction checkFr qui pour un tweet en entrée renvoie vrai ou faux selon si la langue est 'fr'
+        function checkFr(tweet) {
+            return tweet.lang === "fr"
+        }
 
-    /* [12] créer une fonction qui crée et renvoie le bouton de filtre.
-      Cette fonction doit contenir toute la logique liée au filtre.
-      Utiliser cette fonction pour remplacer le code de création du bouton de filtre.
-    */
+        // [X5] modifier "bouton", et utiliser checkFr au clic du bouton pour afficher dans la console la liste des
+        // tweets français
 
-    // ### BONUS: LOCALSTORAGE ###
-    // [1] Rajouter un bouton "fav" à chaque li
+        // [6] modifier "bouton" pour que quand on clique dessus,
+        //  X- crée un nouveau <ol> avec seulement les tweets français
+        //  X- remplace le <ol> existant avec le nouvel <ol>, en utilisant .replaceWith()
 
-    /* [2] quand le bouton est cliqué, changer le style du li (rajouter une classe 'fav')
-      + et stocker l'ensemble des id_str fav dans le localStorage */
+        // [X7] créer une variable booléenne isFr, et l'initialiser à false
+        let idFr = false;
+        // [X8] inverser la valeur du booléen au clic sur "bouton"
 
-    // [3] au chargement de la page, lire le localStorage pour favoriser les favoris.
-  },
-  { once: true },
+        // X[9] modifier "bouton" pour pouvoir réafficher tous les tweets quand on reclique dessus
+        //  XIl faut utiliser isFr.
+
+        // [X10] changer l'intitulé de "bouton" de filtre en fonction de isFr
+
+        /* [11] créer une fonction, qui pour un tableau tweets en entrée, crée et retourne un <ol> rempli de <li>
+        et l'utiliser à [3], [6], [9] */
+
+        function displayTweetList(myArray){
+            const olList = document.createElement("ol");
+            myArray.forEach((items)=>{
+                olList.append(createLi(items))
+            })
+            return olList;
+        }
+        let displayedOL = displayTweetList(tweets);
+
+        document.body.append(displayedOL);
+
+        /* [12] créer une fonction qui crée et renvoie le bouton de filtre.
+          Cette fonction doit contenir toute la logique liée au filtre.
+          Utiliser cette fonction pour remplacer le code de création du bouton de filtre.
+        */
+
+        // ### BONUS: LOCALSTORAGE ###
+        // [X1] Rajouter un bouton "fav" à chaque li
+
+        /* [2] quand le bouton est cliqué, changer le style du li (rajouter une classe 'fav')
+          + et stocker l'ensemble des id_str fav dans le localStorage */
+        function buttonFav(){
+            const fav = document.createElement("button");
+            fav.classList.add("favoris")
+            fav.addEventListener(
+                "click",
+                () => {
+                    fav.classList.toggle("active");
+                    const parent = fav.parentElement;
+                    parent.classList.toggle("active")
+                }
+            )
+            return fav;
+        }
+        // [3] au chargement de la page, lire le localStorage pour favoriser les favoris.
+    },
+    {once: true},
 );
